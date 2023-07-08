7c625331165376064caef3b2c8babdd7ccefa200
OlegPlatonov <o.a.platonov@gmail.com>
2023-07-08 20:14:19 +0300

    add DDP

--- train.py
+++ train.py
@@ -6,9 +6,13 @@ from tqdm import tqdm
 
 import torch
 from torch.nn import functional as F
+from torch import distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
 from torch.cuda.amp import autocast, GradScaler
 from torch.utils.tensorboard import SummaryWriter
+from torch.nn.modules.utils import consume_prefix_in_state_dict_if_present
 
 from models import Transformer, CBoETransformer
 from datasets import TextDataset
@@ -48,7 +52,7 @@ def get_args():
                         help='If None, warmup_proportion is used instead.')
     parser.add_argument('--warmup_proportion', type=float, default=0.1, help='Only used if num_warmup_steps is None.')
 
-    parser.add_argument('--device', type=str, default='cuda:0')
+    parser.add_argument('--device_ids', type=str, default='0,1,2,3,4,5,6,7')
     parser.add_argument('--amp', default=False, action='store_true')
     parser.add_argument('--num_workers', type=int, default=16)
 
@@ -58,11 +62,11 @@ def get_args():
 
 
 def train_epoch(model, data_loader, optimizer, scaler, scheduler, tb_writer, epoch, step, num_processed_samples, device,
-                amp=False, num_accumulation_steps=1):
+                amp=False, num_accumulation_steps=1, rank=0, world_size=1):
     model.train()
     optimizer.zero_grad(set_to_none=True)
-    num_samples = len(data_loader) * data_loader.batch_size
-    with tqdm(total=num_samples, desc=f'Epoch {epoch}') as progress_bar:
+    num_samples = len(data_loader) * data_loader.batch_size * world_size
+    with tqdm(total=num_samples, desc=f'Epoch {epoch}', disable=(rank != 0)) as progress_bar:
         for i, (inputs, targets) in enumerate(data_loader, start=1):
             inputs, targets = inputs.to(device), targets.to(device)
 
@@ -76,14 +80,18 @@ def train_epoch(model, data_loader, optimizer, scaler, scheduler, tb_writer, epo
 
             loss_value = loss.item() * num_accumulation_steps
             cur_lr = scheduler.get_last_lr()[0]
-            cur_batch_size = len(inputs)
+            cur_batch_size = len(inputs) * world_size
             num_processed_samples += cur_batch_size
-            tb_writer.add_scalar(tag='train loss', scalar_value=loss_value, global_step=num_processed_samples)
+
+            if tb_writer is not None:
+                tb_writer.add_scalar(tag='train loss', scalar_value=loss_value, global_step=num_processed_samples)
+
             progress_bar.update(cur_batch_size)
             progress_bar.set_postfix(step=step, lr=f'{cur_lr:.2e}', loss=f'{loss_value:.4f}')
 
             if i % num_accumulation_steps == 0:
-                tb_writer.add_scalar(tag='lr', scalar_value=cur_lr, global_step=num_processed_samples)
+                if tb_writer is not None:
+                    tb_writer.add_scalar(tag='lr', scalar_value=cur_lr, global_step=num_processed_samples)
 
                 scaler.step(optimizer)
                 scaler.update()
@@ -112,8 +120,9 @@ def evaluate(model, data_loader, tb_writer, num_processed_samples, device, amp=F
     loss_mean = loss_sum / num_tokens
     perplexity = exp(loss_mean)
 
-    print(f'Val perplexity: {perplexity:.4f}')
-    tb_writer.add_scalar(tag='val perplexity', scalar_value=perplexity, global_step=num_processed_samples)
+    print(f'Val perplexity: {perplexity:.4f}\n')
+    if tb_writer is not None:
+        tb_writer.add_scalar(tag='val perplexity', scalar_value=perplexity, global_step=num_processed_samples)
 
     return perplexity
 
@@ -121,28 +130,42 @@ def evaluate(model, data_loader, tb_writer, num_processed_samples, device, amp=F
 def main():
     args = get_args()
 
-    save_dir = get_save_dir(base_dir=args.save_dir, name=args.name)
-    print(f'Results will be saved to {save_dir}.')
+    dist.init_process_group('nccl')
+    rank = dist.get_rank()
+    world_size = dist.get_world_size()
 
-    with open(os.path.join(save_dir, 'args.yaml'), 'w') as file:
-        yaml.safe_dump(vars(args), file, sort_keys=False)
+    if rank == 0:
+        save_dir = get_save_dir(base_dir=args.save_dir, name=args.name)
+        print(f'Results will be saved to {save_dir}.')
 
-    tb_writer = SummaryWriter(log_dir=save_dir)
+        with open(os.path.join(save_dir, 'args.yaml'), 'w') as file:
+            yaml.safe_dump(vars(args), file, sort_keys=False)
 
-    logger = Logger(save_dir=save_dir, metric='perplexity', maximize_metric=False)
+        logger = Logger(save_dir=save_dir, metric='perplexity', maximize_metric=False)
 
-    print('Preparing data...')
+        tb_writer = SummaryWriter(log_dir=save_dir)
+
+    else:
+        tb_writer = None
+
+    if rank == 0:
+        print('Preparing data...')
+
     train_dataset = TextDataset(os.path.join('data', f'{args.dataset}_train.txt'))
     val_dataset = TextDataset(os.path.join('data', f'{args.dataset}_val.txt'))
-
-    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, collate_fn=train_dataset.collate_fn,
-                              shuffle=True, drop_last=True, num_workers=args.num_workers, pin_memory=True)
-    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, collate_fn=val_dataset.collate_fn,
+    train_sampler = DistributedSampler(dataset=train_dataset, num_replicas=world_size, rank=rank, shuffle=True,
+                                       drop_last=True)
+    train_loader = DataLoader(dataset=train_dataset, sampler=train_sampler, batch_size=args.batch_size,
+                              collate_fn=train_dataset.collate_fn, drop_last=True, num_workers=args.num_workers,
+                              pin_memory=True)
+    val_loader = DataLoader(dataset=val_dataset, batch_size=args.batch_size, collate_fn=val_dataset.collate_fn,
                             shuffle=False, drop_last=False, num_workers=args.num_workers, pin_memory=True)
 
     num_steps = len(train_loader) // args.num_accumulation_steps * args.num_epochs
 
-    print('Creating model...')
+    if rank == 0:
+        print('Creating model...')
+
     if args.cboe:
         model = CBoETransformer(num_layers=args.num_layers,
                                 cboe_every_layers=args.cboe_every_layers,
@@ -165,13 +188,18 @@ def main():
 
     if args.pretrained_model is not None:
         state_dict = torch.load(args.pretrained_model)
+        consume_prefix_in_state_dict_if_present(state_dict)
 
         del state_dict['output_linear.weight']
         del state_dict['output_linear.bias']
 
         model.load_state_dict(state_dict, strict=False)
 
-    model.to(args.device)
+    device_id = int(args.device_ids.split(',')[rank])
+    device = f'cuda:{device_id}'
+    model.to(device)
+
+    model = DDP(model, device_ids=[device_id], output_device=device_id)
 
     parameter_groups = get_parameter_groups(model)
     optimizer = torch.optim.AdamW(parameter_groups, lr=args.lr, weight_decay=args.weight_decay)
@@ -179,33 +207,38 @@ def main():
     scheduler = get_lr_scheduler(optimizer=optimizer, num_steps=num_steps, num_warmup_steps=args.num_warmup_steps,
                                  warmup_proportion=args.warmup_proportion)
 
-    print('Starting training...')
-    best_perplexity = None
-    best_epoch = None
+    if rank == 0:
+        print('Starting training...')
+        best_perplexity = None
+        best_epoch = None
+
     step = 1
     num_processed_samples = 0
     for epoch in range(1, args.num_epochs + 1):
+        train_sampler.set_epoch(epoch)
         step, num_processed_samples = train_epoch(model=model, data_loader=train_loader, optimizer=optimizer,
                                                   scaler=scaler, scheduler=scheduler, tb_writer=tb_writer,
                                                   epoch=epoch, step=step, num_processed_samples=num_processed_samples,
-                                                  device=args.device, amp=args.amp,
-                                                  num_accumulation_steps=args.num_accumulation_steps)
+                                                  device=device, amp=args.amp,
+                                                  num_accumulation_steps=args.num_accumulation_steps,
+                                                  rank=rank, world_size=world_size)
 
-        perplexity = evaluate(model=model, data_loader=val_loader, tb_writer=tb_writer,
-                              num_processed_samples=num_processed_samples, device=args.device, amp=args.amp)
+        if rank == 0:
+            perplexity = evaluate(model=model, data_loader=val_loader, tb_writer=tb_writer,
+                                  num_processed_samples=num_processed_samples, device=device, amp=args.amp)
 
-        print()
+            logger.update_metrics(val_metric=perplexity, epoch=epoch)
 
-        logger.update_metrics(val_metric=perplexity, epoch=epoch)
+            if best_perplexity is None or perplexity < best_perplexity:
+                best_perplexity = perplexity
+                best_epoch = epoch
+                if args.save_model:
+                    torch.save(model.state_dict(), os.path.join(save_dir, 'model.pt'))
 
-        if best_perplexity is None or perplexity < best_perplexity:
-            best_perplexity = perplexity
-            best_epoch = epoch
-            if args.save_model:
-                torch.save(model.state_dict(), os.path.join(save_dir, 'model.pt'))
+        dist.barrier()
 
-    print(f'Best val perplexity: {best_perplexity} (achieved after epoch {best_epoch})')
-    print()
+    if rank == 0:
+        print(f'Best val perplexity: {best_perplexity} (achieved after epoch {best_epoch})\n')
 
 
 if __name__ == '__main__':
